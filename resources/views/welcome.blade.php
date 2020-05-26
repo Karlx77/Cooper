e9d76cd4aaa88212741db21af10a8cfab86a4219
AssielMadai <leisamada@gmail.com>
2020-05-26 14:19:32 +0000

    articulos2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
-                height: 100vh;
+                height: 200vh;
                 margin: 0;
             }
 
@@ -105,7 +105,50 @@
         </div>
 
     </div>
-        <main class="container-fluid">
+    <div  class="container"><br>
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Bolivia Segura</h5>
+                        <p class="card-text">En este sitio web se puede encontrar información actualizada sobre la enfermedad, recomendaciones dirigidas a los distintos sectores de la población y las diferentes medidas para atender la pandemia y sus efectos.</p>
+                        <a href="https://www.boliviasegura.gob.bo" class="btn btn-primary">Ir a la Pagina</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">OMS</h5>
+                        <p class="card-text">Anuncio Oficial desde la Organización Mundial de la Salud sobre la COVID-19. Síntomas, Prevención, Advertencia de Viaje y Preguntas Frecuentes sobre la COVID-19. Cómo protegerse. Orientaciones Técnicas. Consejos e información. Preguntas y Respuestas.</p>
+                        <a href="https://www.who.int/es/emergencies/diseases/novel-coronavirus-2019?gclid=CjwKCAjw2a32BRBXEiwAUcugiIPm1sP21_rP1iyaBq1niVQgMnl1f0yjrW0L_xPTGr0S_XMmzb2A_hoCKT8QAvD_BwE" class="btn btn-primary">Ir a la pagina</a>
+                    </div>
+                </div>
+            </div>
+        </div><br>
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <main class="container-fluid">
             @yield('content')
         </main>
     @yield('principal')
